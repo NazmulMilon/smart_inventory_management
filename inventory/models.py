b5a7cae557ca21f08e64b171835bbcaf5efa03e4
Milon73 <milon16103373@gmail.com>
2021-10-24 21:09:28 +0600

    first commit

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.name
+
+class Authentication(models.Model):
+    user=models.ForeignKey(UserProfile, on_delete=models.CASCADE, null=True, blank=True)
+    phone=models.CharField(max_length=11)
+    password=models.IntegerField()
+
